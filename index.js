e7f5287610d767d1e8390afa51c265e26db61cd4
Robert Kowalski <rok@kowalski.gd>
2013-07-14 19:16:29 +0200

    implement csv stream

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 var Transform = require('stream').Transform || require('readable-stream').Transform
 var fs = require('fs')
 
-module.exports = transformAndWrite
-function transformAndWrite (target) {
+module.exports = transform
+function transform (d, l, p) {
 
-  return writer.pipe(logWriter(target))
+  var stream = new Transform()
+
+  var first = true
+  var time = new Buffer(p || '' + new Date().getTime(), 'utf8')
+  d = new Buffer(d || ';', 'utf8')
+  l = new Buffer(l || '\n', 'utf8')
+
+  stream._transform = function (chunk, enc, cb) {
+
+    if (first) {
+      first = false
+      stream.push(time)
+      stream.push(d)
+    }
+    stream.push(chunk)
+
+    cb()
+  }
+
+  stream._flush = function (cb) {
+    stream.push(l)
+    cb()
+  }
+
+  return stream
 }
-
-function logWriter (target) {
-  return fs.createWriteStream(target, {'flags': 'a'});
-}
-
